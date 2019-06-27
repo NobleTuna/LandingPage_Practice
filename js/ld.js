f0ad5ac308379a7c62288e89864500626b89e369
NobleTuna <lyy7661@naver.com>
2019-06-27 17:21:31 +0900

    add typing effect

--- js/ld.js
+++ js/ld.js
@@ -1,10 +1,28 @@
 if (typeof ssafy === "undefined") {
-    ssafy = {};
+  ssafy = {};
 }
 
 ssafy.ld = {
   init: function() {
     ssafy.ld.scrollAnimation();
+    var typingBool = false;
+    var typingIdx = 0;
+    var typingTxt = $(".typing-txt").text(); // 타이핑될 텍스트를 가져온다
+    typingTxt = typingTxt.split(""); // 한글자씩 자른다.
+    if (typingBool == false) { // 타이핑이 진행되지 않았다면
+      typingBool = true;
+
+      var tyInt = setInterval(typing, 100); // 반복동작
+    }
+
+    function typing() {
+      if (typingIdx < typingTxt.length) { // 타이핑될 텍스트 길이만큼 반복
+        $(".typing").append(typingTxt[typingIdx]); // 한글자씩 이어준다.
+        typingIdx++;
+      } else {
+        clearInterval(tyInt); //끝나면 반복종료
+      }
+    }
   },
 
   // 헤더 스크롤 애니메이션
@@ -44,6 +62,8 @@ ssafy.ld = {
     let headerHeight = document.querySelector(".header-wrapper");
     let itemsOffset = pages[n].offsetTop + headerHeight.offsetHeight;
 
-    body.stop().animate({ scrollTop: itemsOffset - 78 }, 500, "swing");
+    body.stop().animate({
+      scrollTop: itemsOffset - 78
+    }, 500, "swing");
   },
 };
